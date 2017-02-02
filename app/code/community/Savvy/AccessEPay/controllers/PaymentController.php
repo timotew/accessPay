f63ef5d88a3e96a0ddd5bdcdb5aba162294128c8
timotew <timotewpeters@gmail.com>
2017-02-02 10:02:06 +0100

    Introduction of Ajax Verify Account Button to the admin Interface.

--- app/code/community/Savvy/AccessEPay/controllers/PaymentController.php
+++ app/code/community/Savvy/AccessEPay/controllers/PaymentController.php
@@ -13,11 +13,51 @@ class Savvy_AccessEPay_PaymentController extends Mage_Core_Controller_Front_Acti
 
             Mage_Core_Controller_Varien_Action::_redirect('accessepay/payment/response', array('_secure' => false, '_query'=> $arr_querystring));
         }
+
         /*
          * TODO: Remove this the gateway action is performed by access getway URL
          */
     }
+    public function verifyAccountAction()
+    {
+        $params =  $this->getRequest()->getPost();
+        $helper =  Mage::helper('accessepay');
+        $response = Mage::app()->getResponse()
+            ->setHeader('content-type', 'application/json; charset=utf-8');
 
+        $result = array(
+            'status' => 0,
+            'description' => $this->__('An error has occurred. Please, contact the store administrator.')
+        );
+        $merchantInfo = $helper->getgetMerchantDetails();
+
+
+
+        foreach ($merchantInfo as $key => $val) {
+            if (empty($params[$key]) && $params[$key] !== 0) {
+                $result['description'] = $this->__(
+                    'Merchant ID, Merchant Email and Currency Code are required in order to verify your account'
+                );
+                $response->setBody(json_encode($result));
+
+                return;
+            }
+        }
+
+        if (!$params['merchant_id_changed'] && $params['merchant_id'] == '******') {
+            $params['merchant_id'] = $helper->getConfigData('merchant_id', 'payment_accessepay');
+            $params['merchant_id'] = Mage::helper('core')->decrypt($params['merchant_id']);
+        }
+
+        if (!$params['merchant_email'] && $params['password'] == '******') {
+            $params['merchant_email'] = $helper->getConfigData('merchant_email', 'payment_accessepay');
+            $params['merchant_email'] = Mage::helper('core')->decrypt($params['merchant_email']);
+        }
+
+        $result = Mage::getModel('accessepay/status')->verifyAccount($params);
+
+        $response->setBody(json_encode($result));
+    }
     public function redirectAction()
     {
         /*
